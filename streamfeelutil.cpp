4d15b9e5510a60c2a6eea9d7732624bd14430415
Adam Browne <adamo.browne@gmail.com>
2017-03-09 16:23:01 -0500

    add serialization utility

---
+++ streamfeelutil.cpp
@@ -0,0 +1,170 @@
+/* StreamFeel Dataset Utility/Serializer (Author: Adam Browne)
+ * This utility encodes the dataset and adds new records to existing datasets.
+ * the serialized data we get from the trained model is used in the actual extension. */
+
+// text_categorizer.h was modified to include "encode / decode" to use the serialization/deserialization.
+
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+#include "../../../mitielib/include/mitie/text_categorizer.h"
+#include "../../../mitielib/include/mitie/text_categorizer_trainer.h"
+
+using namespace mitie;
+using namespace dlib;
+
+//reads the contents of the file as a string.
+std::string readData(std::ifstream& fileReader){
+	std::stringstream buffer;
+	buffer << fileReader.rdbuf();
+	return buffer.str();
+}
+
+/* In order to categorize a msg, we must first tokenize it.
+* this helper function does that by parsing spaces. */
+std::vector<std::string> tokenize_msg(std::string& input, char delim){
+    std::vector<std::string> tokens;
+        tokens.reserve(input.size());
+        int curSpace = input.find(delim), curPos = 0;
+        while(curSpace != std::string::npos){
+            tokens.push_back(input.substr(curPos,curSpace-curPos));
+            curPos = curSpace+1;
+            curSpace = input.find(delim,curPos);      
+        }
+        //if no space, or last region, push it in.
+        tokens.push_back(input.substr(curPos));
+        return tokens;
+}
+
+//split by commas
+std::vector<std::string> split(std::string& raw){
+	return tokenize_msg(raw,',');
+}
+
+/* Load an existing dataset from a file. */
+std::vector<std::string> dataset(char which){
+	//used to hold data extracted from the file.
+	std::string raw;	
+	if(which == 'r'){
+		std::ifstream fileReader("relevance.txt");
+		raw = readData(fileReader);
+	} else {
+		std::ifstream fileReader("sentiment.txt");
+		raw = readData(fileReader);
+	}
+	return split(raw);
+}
+
+std::vector<char> serialize(char choice) {
+    //the executor we're using to encode our dataset.
+    text_categorizer executor;
+    //pull the dataset from the file.
+    std::vector<std::string> data = dataset(choice);
+    text_categorizer_trainer fit;
+    int i = 0, invariant = data.size() - 1;
+    //add the <msg,label> pair to the trainer.
+    while(i < invariant){
+        std::string label = data[i+1];
+        std::string msg = data[i];
+        std::vector<std::string> token = tokenize_msg(msg,' ');
+        fit.add(token,label);
+        i += 2;
+    }
+    fit.set_num_threads(4);
+    executor = fit.train();
+    //here, we use a vectorstream to serialize the categorizer.
+    //the serialized buffer is present in buf afterwards.
+    std::vector<char> buf;
+    dlib::vectorstream strm(buf);
+    executor.encode(executor,strm);
+    return buf;
+}
+
+
+void addRecord(char choice) {
+	std::string file, lbl;
+	if(choice == 'r'){
+		lbl = "Relevant (y/n):\n";
+		file = "relevance.txt";
+	} else {
+		lbl = "Sentiment:\n";
+		file = "sentiment.txt";
+	}
+	//open the dataset for appending because we're adding another record.	
+	std::ofstream fileWriter;
+	fileWriter.open(file, std::ios_base::app);
+	//take the input for the message / label	
+	std::string msg, label;
+	std::cout << "Enter message:\n";
+	std::cin.ignore();
+	std::getline(std::cin,msg);
+	std::cout << lbl;
+	std::cin >> label;
+	//need the delimeter ',': can't confuse it.
+	msg.erase(std::remove(msg.begin(), msg.end(), ','), msg.end());
+	//append the new record to the dataset.
+	std::string new_msg = "," + msg + "," + label;
+	fileWriter << new_msg;
+	std::cout << "Added " << msg << "," << label << std::endl;
+}
+
+void displayMenu() {
+	std::cout << "1. Add Relevance\n";
+	std::cout << "2. Add Sentiment\n";
+	std::cout << "3. Update Relevance\n";
+	std::cout << "4. Update Sentiment\n";		
+	std::cout << "5. Display Menu\n";
+}
+
+/* Serializes a trained categorizer model and writes its
+ * raw representation to the javascript file containing it,
+ * this will be deserialized by the native client module. */
+void updateDataset(char choice){
+	//get the trained buffer (raw representation of the model)
+	std::vector<char> buffer = serialize(choice);
+	std::string var_name, file_name;
+	if(choice == 'r'){
+		var_name = "relevance_data = [\n";
+		file_name = "/Users/adambrowne/Desktop/Personal/StreamFeel/src/inject/default.js";
+	} else {
+		var_name = "sentiment = [\n";
+		file_name = "/Users/adambrowne/Desktop/Personal/StreamFeel/src/inject/sentiment.js";
+	}
+	std::string updated_data = "var " + var_name;
+	for(int byte : buffer)
+		updated_data += (to_string(byte) + ",");
+	updated_data.pop_back(); //kill trailing comma.
+	updated_data += "\n];";
+	std::ofstream dataWriter(file_name);
+	dataWriter << updated_data;
+}
+//1 - Update Rel, 2 - Update Sent, 3 - Add Rel, 4 - Add Sent - 5: display menu
+int main() {
+	int choice; //1 - 4:	
+	displayMenu();
+	do {
+		std::cin >> choice;
+		switch(choice){
+			case 1:
+				addRecord('r');
+				break;
+			case 2:
+				addRecord('s');
+				break;
+			case 3:
+				updateDataset('r');
+				break;
+			case 4:
+				updateDataset('s');
+				break;
+			case 5:
+				displayMenu();
+				break;
+		}
+	} while(choice >= 1 && choice <= 5);
+}
+
