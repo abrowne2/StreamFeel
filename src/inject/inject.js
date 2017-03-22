fd382c31604e53027210aae9066233074b2aa05f
Adam Browne <adamo.browne@gmail.com>
2017-03-21 22:27:32 -0400

    Begin sentiment chart implementation

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,11 +4,24 @@
 /* To observe the new messages, we need to use a MutationObserver
 * and then apply a queryselector to get the new message added:
 we're looking for: <li class="message-line chat-line ember-view"> */
-	var toggle_filter = false;
+	var toggle_filter = false, pie = null;
+	var sentData = [
+	  { label: "1", value: 6 },
+	  { label: "2", value: 4 },
+	  { label: "3", value: 3 },
+	  {label:"4",value:2},
+	  {label:"5",value:1},
+	  {label:"6",value:5},
+	  {label:"7",value:7},
+	  {label:"8",value:3},
+	  {label:"9",value:9}
+	];
+    //listener to the popup menu. We listen to it's instructions.
     chrome.runtime.onMessage.addListener(function(response){
         if(response == "tf")
             toggleFilter();
     });    
+
     var port = chrome.runtime.connect({name: "handler"});
     port.onMessage.addListener(function(message){
         var data = message.split("|");
@@ -16,9 +29,7 @@ we're looking for: <li class="message-line chat-line ember-view"> */
         if(data[3] == "1") {//TODO: Non-relevant display CONFIGURE BY POPup
             try {
                 target_msg.setAttribute("style", "display:block;visibility:visible;");
-            } catch(err){
-
-            }
+            } catch(err) {}
         } else {
             try {
                 target_msg.setAttribute("style", "display:none;visibility:hidden;");
@@ -37,6 +48,10 @@ we're looking for: <li class="message-line chat-line ember-view"> */
     var getChatBoxElement = function(main) {
         if(document.querySelector(main) != null){
             setupMessageListener(document.querySelector(main));
+            var sentChart = setupDataViz();
+            if(sentChart != null){
+            	setupChart(sentChart);
+            }            
         } else {
             setTimeout(function() {
                 getChatBoxElement(main);
@@ -75,7 +90,7 @@ we're looking for: <li class="message-line chat-line ember-view"> */
                             if(curMsg.id == "" || curMsg.id == "undefined")
                                 curMsg.setAttribute("id", curMsg.getAttribute("data-id"));
                             need.push(curMsg.id);
-                        } catch(err) { }
+                        } catch(err) {}
                     }
                 }
             })
@@ -84,17 +99,118 @@ we're looking for: <li class="message-line chat-line ember-view"> */
         observer.observe(chat_box, {childList: true});
     }
 
+    //if we're turning the filter back on, process all queued messages.
     function toggleFilter() {
     	toggle_filter = toggle_filter == false? true: false;
         if(toggle_filter == false){
             while(need.length > 0){
                 try {
                     handleTwitchMsg(document.getElementById(need.shift()));
-                } catch(err){
-
-                }
+                } catch(err){}
             }
         }
+    }
+
+    function setupDataViz() {
+    	var frame = document.getElementById("dataviz");
+    	var sentiment = null;
+    	if(frame == "" || frame == "undefined" || frame == null){
+    		var structure = document.createElement("div");
+    		// structure.style.visibility = "hidden";
+    		structure.setAttribute("id","dataviz");
+    		var styling = "position:absolute;top:50%;left:50%;margin-top:-250px;margin-left:-400px;width:800px;height:500px;z-index:9002;border-radius:3px;border: 1px solid #000;";
+    		structure.setAttribute("style", styling);
+			sentiment = document.createElement("div");
+			sentiment.setAttribute("id","sent");
+    		structure.appendChild(sentiment);
+    		var top = document.getElementsByTagName("body")[0];
+    		if(top) top.appendChild(structure);
+    	}
+    	return sentiment;
+    }
+
+    function setupChart(construct) {
+        // document.getElementById("dataviz").style.visibility = "visible";
+        // var construct = document.getElementById("sent");
+		pie = new d3pie(construct, {
+			"header": {
+				"title": {
+					"text": "What people are feeling",
+					"fontSize": 22,
+					"font": "verdana"
+				},
+				"subtitle": {
+					"text": "General sentiment by the minute",
+					"color": "#999999",
+					"fontSize": 10,
+					"font": "verdana"
+				},
+				"titleSubtitlePadding": 12
+			},
+			"footer": {
+				"text": "generated using ",
+				"color": "#999999",
+				"fontSize": 11,
+				"font": "open sans",
+				"location": "bottom-center"
+			},
+			"size": {
+				"canvasHeight": 400,
+				"canvasWidth": 590,
+				"pieOuterRadius": "96%"
+			},
+			"data": {
+				"content": sentData
+			},
+			"labels": {
+				"mainLabel": {
+					"color": "#050506",
+					"font": "verdana",
+					"fontSize": 11
+				},
+				"percentage": {
+					"color": "#ffffff",
+					"font": "verdana",
+					"fontSize": 11,
+					"decimalPlaces": 0
+				},
+				"value": {
+					"color": "#e1e1e1",
+					"font": "verdana"
+				},
+				"lines": {
+					"enabled": true,
+					"color": "#cccccc"
+				},
+				"truncation": {
+					"enabled": true
+				}
+			},
+			"effects": {
+				"load": {
+					"speed": 450
+				},
+				"pullOutSegmentOnClick": {
+					"effect": "linear",
+					"speed": 400,
+					"size": 8
+				}
+			},
+			"misc": {
+				"colors": {
+					"segmentStroke": "",
+					segments: [
+						"#4D4D4D","#5DA5DA","#FAA43A","#60BD68","#F17CB0","#B2912F","#B276B2","#DECF3F","#F15854"
+					]
+				},
+				"canvasPadding": {
+					"top": 2,
+					"right": 2,
+					"bottom": 2,
+					"left": 2
+				}
+			}
+		});        // pie.redraw();
     }
 
     var oldStream = "";
@@ -103,6 +219,7 @@ we're looking for: <li class="message-line chat-line ember-view"> */
         if(curStream != oldStream){
             oldStream = curStream;
             getChatBoxElement("ul.chat-lines");
+
         }
         oldStream = window.location.href;
         setTimeout(function() {
@@ -115,22 +232,6 @@ we're looking for: <li class="message-line chat-line ember-view"> */
 // var tester = false, pie;
 // ready("div.mg-b-2", function(element){
 //     if(tester == false){
-//         pie = new d3pie(element, {
-//             header: {
-//                 title: {
-//                     text: "A very simple example pie"
-//                 }
-//             },
-//             data: {
-//                 content: [
-//                     { label: "JavaScript", value: 264131 },
-//                     { label: "Ruby", value: 218812 },
-//                     { label: "Java", value: 157618},
-//                 ]
-//             }
-//         });
-//         tester = true;
-//         pie.redraw();
 //     }
 // });
 
