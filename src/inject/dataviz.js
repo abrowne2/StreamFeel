c965c28cd96ec98553a74eb4540f0e95e1b5b3d6
Adam Browne <adamo.browne@gmail.com>
2017-03-29 14:44:12 -0400

    refactor chart code into other file..

---
+++ src/inject/dataviz.js
@@ -0,0 +1,135 @@
+/* Author: Adam Browne, w/ lines 109-133: jnoreiga; stackoverflow(seebelow)
+ * Dataviz.js sets up the data visualization (analytics), for 
+ * commands, emojis, and sentiment. */
+
+var styling = "position:absolute;top:50%;left:50%;margin-top:-250px;margin-left:-400px;width:450px;height:350px;z-index:9002; +\
+				border-radius:3px;border: 1px solid #000;background-color:mintcream;cursor:pointer;";
+
+function setupDataViz() {
+	var frame = document.getElementById("dataviz");
+	var sentiment = null;
+	if(frame == "" || frame == "undefined" || frame == null){
+		var structure = document.createElement("div");
+		// structure.style.visibility = "hidden";
+		structure.setAttribute("id","dataviz");
+		structure.setAttribute("style", styling);
+		sentiment = document.createElement("div");
+		sentiment.setAttribute("id","sent");
+		structure.appendChild(sentiment);
+		var top = document.getElementsByTagName("body")[0];
+		if(top) top.appendChild(structure);
+		setupDrag();
+	}
+	return sentiment;
+}
+
+//arbitrary dummy data for the chart rendering.
+var analytics = [{ label: "1", value: 6 },{ label: "2", value: 4 },{ label: "3", value: 3 },{label:"4",value:2},
+					{label:"5",value:1},{label:"6",value:5},{label:"7",value:7},{label:"8",value:3},{label:"9",value:9}];
+
+
+function chartSettings() {
+	return {
+		"header": {
+			"title": {
+				"text": "What people are feeling / thinking",
+				"fontSize": 20,
+				"font": "verdana"
+			},
+			"subtitle": {
+				"text": "General sentiment by the minute",
+				"color": "#999999",
+				"fontSize": 9,
+				"font": "verdana"
+			},
+			"titleSubtitlePadding": 12
+		},
+		"footer": {
+			"text": "generated using ",
+			"color": "#999999",
+			"fontSize": 11,
+			"font": "open sans",
+			"location": "bottom-center"
+		},
+		"size": {
+			"canvasHeight": 300,
+			"canvasWidth": 443,
+			"pieOuterRadius": "100%"
+		},
+		"data": {
+			"content": analytics
+		},
+		"labels": {
+			"mainLabel": {
+				"color": "#050506",
+				"font": "verdana",
+				"fontSize": 11
+			},
+			"percentage": {
+				"color": "#ffffff",
+				"font": "verdana",
+				"fontSize": 10,
+				"decimalPlaces": 0
+			},
+			"value": {
+				"color": "#e1e1e1",
+				"font": "verdana"
+			},
+			"lines": {
+				"enabled": true,
+				"color": "#cccccc"
+			},
+			"truncation": {
+				"enabled": true
+			}
+		},
+		"effects": {
+			"load": {
+				"effect": "none"
+			},
+			"pullOutSegmentOnClick": {
+				"effect": "linear",
+				"speed": 400,
+				"size": 8
+			}
+		},
+		"misc": {
+			"colors": {
+				"segmentStroke": "",
+				segments: [
+					"#5DA5DA","#FAA43A","#60BD68","#F17CB0","#B2912F","#B276B2","#DECF3F","#F15854","#2ECCC4"
+				]
+			},
+			"canvasPadding": {
+				"top": 2, "right": 2, "bottom": 2, "left": 2
+			}
+		}
+	};    
+}
+
+//Lines 109-133 credit jnoreiga; http://stackoverflow.com/questions/9334084/moveable-draggable-div
+var x_pos = 0, y_pos = 0;
+
+function setupDrag() {
+  var sent = document.getElementById('dataviz');
+  sent.addEventListener('mousedown', mouseDown, false);
+  window.addEventListener('mouseup', mouseUp, false);
+}
+
+function mouseUp() {
+  window.removeEventListener('mousemove', divMove, true);
+}
+
+function mouseDown(e) {
+	var div = document.getElementById('dataviz');
+	x_pos = e.clientX - div.offsetLeft;
+	y_pos = e.clientY - div.offsetTop;
+	window.addEventListener('mousemove', divMove, true);
+}
+
+function divMove(e) {
+  var div = document.getElementById('dataviz');
+  div.style.position = 'absolute';
+  div.style.top = (e.clientY - y_pos) + 'px';
+  div.style.left = (e.clientX - x_pos) + 'px';
+}
